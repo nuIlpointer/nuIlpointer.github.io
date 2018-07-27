140fea01d4e57988c4e7edef815056fea2b92cae
nuIlpointer(ぬいるぽいんた) <serval_chan@hotmail.com>
2018-07-27 22:32:51 +0900

    サイトデザイン更新とアイコン追加
    めんどくさかったです

---
+++ res/more.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+  var options = new Object;
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+//ここからconsole出力
+console.log("このデバッグページを開くより https://materializecss.com/ を見た方が有益です。");
+console.log("保存して察しのついた方、主の別ページの使いまわしのHPです\n手を抜いてはいけない(戒め)");
+
