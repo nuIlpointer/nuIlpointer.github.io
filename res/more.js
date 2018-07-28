e16c9790f0a6588cc271efec481055aa665a8364
nuIlpointer(ぬいるぽいんた) <serval_chan@hotmail.com>
2018-07-28 13:00:11 +0900

    footerとheaderいじり
    誰もサイトを見ないので実験台にします

--- res/more.js
+++ res/more.js
@@ -8,7 +8,14 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
+$(function(){
+  $("#footer").load("res/footer.html");
+});
+
+$(function(){
+  $("#header").load("res/header.html");
+});
+
 //ここからconsole出力
 console.log("このデバッグページを開くより https://materializecss.com/ を見た方が有益です。");
 console.log("保存して察しのついた方、主の別ページの使いまわしのHPです\n手を抜いてはいけない(戒め)");
-
