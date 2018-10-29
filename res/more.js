82c7a7d8cd8abd1b71d3530445ccc1885bad014e
nuilpointer <nuilpointer@outlook.jp>
2018-10-30 01:48:53 +0900

    fix
    知らん

--- res/more.js
+++ res/more.js
@@ -20,6 +20,15 @@ $(document).ready(function(){
     $('.tabs').tabs();
   });
 
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(window).scrollTop()>580) {
+      $('nav').addClass('grey darken-3');
+    } else {
+      $('nav').removeClass('grey darken-3');
+    }
+  });
+});
 //ここからconsole出力
 console.log("このデバッグページを開くより https://materializecss.com/ を見た方が有益です。");
 console.log("保存して察しのついた方、主の別ページの使いまわしのHPです\n手を抜いてはいけない(戒め)");
